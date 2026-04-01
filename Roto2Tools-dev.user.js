5a9aeac92a1e26c802671e6b372adaa54123473f
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2026-04-01 21:38:39 +0200

    1.8.4.1d

--- Roto2Tools-dev.user.js
+++ Roto2Tools-dev.user.js
@@ -8,7 +8,7 @@
 // @icon            https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-48x48.png
 // @icon64          https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-64x64.png
 // @updateURL       https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/Roto2Tools-dev.user.js
-// @version         1.8.4d
+// @version         1.8.4.1d
 // @encoding        UTF-8
 // @match           *://www.forocoches.com/*
 // @match           *://forocoches.com/*
@@ -20,9 +20,9 @@
 // @grant           GM_getMetadata
 // @grant           GM_getResourceText
 // @run-at          document-end
-// @resource        bootstrapcss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.8.4d
-// @resource        Roto2Toolscss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.8.4d
-// @resource        toastcss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/toastr.min.css?v=1.8.4d
+// @resource        bootstrapcss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.8.4.1d
+// @resource        Roto2Toolscss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.8.4.1d
+// @resource        toastcss https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/refs/heads/dev-2/resources/require/toastr.min.css?v=1.8.4.1d
 // ==/UserScript==
 
 (function () {
@@ -77,10 +77,9 @@
         },
         applyButtonInteractionEffects(btn) {
             if (!btn) return;
-            const SHADOW = '0px 2px 4px #000000';
-            btn.addEventListener('mousedown', () => { btn.style.boxShadow = 'none'; btn.style.transform = 'translateY(3px)'; });
-            btn.addEventListener('mouseup', () => { btn.style.boxShadow = SHADOW; btn.style.transform = 'none'; });
-            btn.addEventListener('mouseleave', () => { btn.style.boxShadow = SHADOW; btn.style.transform = 'none'; });
+            btn.addEventListener('mousedown', () => { btn.style.transform = 'translateY(3px)'; });
+            btn.addEventListener('mouseup', () => { btn.style.transform = 'none'; });
+            btn.addEventListener('mouseleave', () => { btn.style.transform = 'none'; });
         },
         applyTooltip(el, content) {
             if (el) el.title = content;
