a6ca7addbcefacd6f8093e3aedbbd79be4a6f779
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2026-04-06 21:06:30 +0200

    1.9.1-3d

--- Roto2Tools-dev.user.js
+++ Roto2Tools-dev.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @id              Roto2Tools
-// @name            Roto2Tools
-// @namespace       Roto2Tools
+// @id              Roto2Tools DEV
+// @name            Roto2Tools DEV
+// @namespace       Roto2Tools DEV
 // @author          DeciBelioS
 // @homepage        https://github.com/Deci8BelioS/Roto2Tools/
 // @description     Script para Forocoches.
 // @icon            https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-48x48.png
 // @icon64          https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-64x64.png
 // @updateURL       https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/Roto2Tools-dev.user.js
-// @version         1.9.1-2d
+// @version         1.9.1-3d
 // @encoding        UTF-8
 // @match           *://www.forocoches.com/*
 // @match           *://forocoches.com/*
@@ -20,18 +20,18 @@
 // @grant           GM_getMetadata
 // @grant           GM_getResourceText
 // @run-at          document-end
-// @resource        bootstrapcss    https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.9.1-2d
-// @resource        Roto2Toolscss   https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.9.1-2d
-// @resource        toastcss        https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/toastr.min.css?v=1.9.1-2d
-// @resource        cust0mMensajes  https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/cust0mMensajes.css?v=1.9.1-2d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/settings.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/utils.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/theme.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/history.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.dropdowns.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.menu.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/processing.js
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/main.js
+// @resource        bootstrapcss    https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.9.1-3d
+// @resource        Roto2Toolscss   https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.9.1-3d
+// @resource        toastcss        https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/toastr.min.css?v=1.9.1-3d
+// @resource        cust0mMensajes  https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/cust0mMensajes.css?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/settings.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/utils.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/theme.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/history.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.dropdowns.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.menu.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/processing.js?v=1.9.1-3d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/main.js?v=1.9.1-3d
 // ==/UserScript==
 
 (function () {
