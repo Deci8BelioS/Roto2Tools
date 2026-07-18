639887b558859908097ece03126145fd618d982f
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2026-07-18 16:00:59 +0200

    Fix CSS cust0mMensajes en posts de alguien que esté como invitado

--- Roto2Tools-dev.user.js
+++ Roto2Tools-dev.user.js
@@ -8,7 +8,7 @@
 // @icon            https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-48x48.png
 // @icon64          https://raw.githubusercontent.com/Deci8BelioS/Roto2Tools/dev/resources/img/icon-64x64.png
 // @updateURL       https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/Roto2Tools-dev.user.js
-// @version         1.9.1-3d
+// @version         1.9.1-4d
 // @encoding        UTF-8
 // @match           *://www.forocoches.com/*
 // @match           *://forocoches.com/*
@@ -20,18 +20,18 @@
 // @grant           GM_getMetadata
 // @grant           GM_getResourceText
 // @run-at          document-end
-// @resource        bootstrapcss    https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.9.1-3d
-// @resource        Roto2Toolscss   https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.9.1-3d
-// @resource        toastcss        https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/toastr.min.css?v=1.9.1-3d
-// @resource        cust0mMensajes  https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/cust0mMensajes.css?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/settings.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/utils.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/theme.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/history.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.dropdowns.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.menu.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/processing.js?v=1.9.1-3d
-// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/main.js?v=1.9.1-3d
+// @resource        bootstrapcss    https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/bootstrapcss.css?v=1.9.1-4d
+// @resource        Roto2Toolscss   https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/Roto2Toolscss.css?v=1.9.1-4d
+// @resource        toastcss        https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/toastr.min.css?v=1.9.1-4d
+// @resource        cust0mMensajes  https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/require/cust0mMensajes.css?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/settings.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/utils.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/theme.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/history.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.dropdowns.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/ui.menu.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/processing.js?v=1.9.1-4d
+// @require         https://github.com/Deci8BelioS/Roto2Tools/raw/refs/heads/dev-2/resources/src/main.js?v=1.9.1-4d
 // ==/UserScript==
 
 (function () {
